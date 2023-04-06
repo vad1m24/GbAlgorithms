8e8c6c72d659d2add698ecc364dc792bb44fdc24
anastasia.rutkevich <VTB4058850@region.vtb.ru>
2023-04-06 23:52:41 +0300

    First task

--- src/task1/Main.java
+++ src/task1/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
 
         HeapSort hp = new HeapSort();
-        int[] arr = hp.makingArray(10,100);
+        int[] arr = hp.makingArray(15,100);
         System.out.println(Arrays.toString(arr));
         System.out.println(Arrays.toString(hp.sort(arr)));
 
